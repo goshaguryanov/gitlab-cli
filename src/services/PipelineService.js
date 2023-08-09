2bb679bb0e675a01a3fa54a90a01d8b34d461f58
gguryanov@webmonitorx.ru <gguryanov@webmonitorx.ru>
2023-08-10 01:57:44 +0300

    WIP

--- src/services/PipelineService.js
+++ src/services/PipelineService.js
@@ -13,10 +13,10 @@ const PipelineService = () => {
             return ApiService.fetch(`/projects/${projectId}/pipelines?${urlParams.toString()}`, { method: 'GET' })
         },
         get: async (projectId, id, entity) => ApiService.fetch(`/projects/${projectId}/pipelines/${id}/${entity ? `/${entity}` : ''}`, { method: 'GET' }),
-        create: async (projectId, ref) => ApiService.fetch(`/projects/${projectId}/pipeline?ref=${ref}`, {
+        create: async (projectId, ref, params) => ApiService.fetch(`/projects/${projectId}/pipeline?ref=${ref}`, {
             method: 'POST',
             body: JSON.stringify({
-                variables: []
+                variables: params
             })
         }),
         update: async (projectId, id, action) => ApiService.fetch(`/projects/${projectId}/pipelines/${id}/${action ? `/${action}` : ''}`, {
